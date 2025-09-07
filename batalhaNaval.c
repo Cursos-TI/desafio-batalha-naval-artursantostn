d12a61a039f48032e3c41e1adcb7a51820b9c467
Artur Santos <artur.santos.tn1@gmail.com>
2025-09-06 23:39:31 -0300

    Add Navios Diagonais batalhaNaval.c
    Adicionado a função para colocar Navios nas Diagonais.
    
    Uma na Diagonal Principal, e a Segunda na Diagonal Secundaria.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,11 +36,33 @@
 // 1 1 1 1 1
 // 0 0 1 0 0
 
-void DEFINIR_TAMANHO_TABULEIRO(int array [10][10]) {
+#define TAMANHO 10
 
-    for (int index_linha = 0; index_linha < 10; index_linha++) {
+// Navio Horizontal (linha fixa, colunas variam)
+#define NAVIO_H_COL_INICIAL 2
+#define NAVIO_H_COL_FINAL 4
+#define NAVIO_H_LINHA 8
 
-        for (int index_coluna = 0; index_coluna < 10; index_coluna++) {
+// Navio Vertical (linhas variam, coluna fixa)
+#define NAVIO_V_LINHA_INICIAL 4
+#define NAVIO_V_LINHA_FINAL 6
+#define NAVIO_V_COLUNA 6
+
+// Navio Diagonal Principal (↘)
+#define NAVIO_DP_LINHA_INICIAL 0
+#define NAVIO_DP_COL_INICIAL 0
+#define NAVIO_DP_TAMANHO 3
+
+// Navio Diagonal Secundária (↙)
+#define NAVIO_DS_LINHA_INICIAL 0
+#define NAVIO_DS_COL_INICIAL 9
+#define NAVIO_DS_TAMANHO 3
+
+void DEFINIR_TAMANHO_TABULEIRO(int array [TAMANHO][TAMANHO]) {
+
+    for (int index_linha = 0; index_linha < TAMANHO; index_linha++) {
+
+        for (int index_coluna = 0; index_coluna < TAMANHO; index_coluna++) {
             
             array[index_linha][index_coluna] = 0;
 
@@ -50,18 +72,31 @@ void DEFINIR_TAMANHO_TABULEIRO(int array [10][10]) {
 
 }
 
-void DEFINIR_LOCAL_NAVIO(int array [10][10]) {
+void DEFINIR_LOCAL_NAVIO(int array [TAMANHO][TAMANHO]) {
 
-    for (int j = 3; j <= 5; j++) {
-        array[2][j] = 3;
+    // Navio na Horizontal
+    for (int j = NAVIO_H_COL_INICIAL; j <= NAVIO_H_COL_FINAL; j++) {
+        array[NAVIO_H_LINHA][j] = 3;
     }
 
-    for (int i = 5; i <= 7; i++) {
-        array[i][7] = 3;
+    // Navio na Vertical
+    for (int i = NAVIO_V_LINHA_INICIAL; i <= NAVIO_V_LINHA_FINAL; i++) {
+        array[i][NAVIO_V_COLUNA] = 3;
     }
+
+    // Navio na Diagonal Principal
+    for (int k = 0; k < NAVIO_DP_TAMANHO; k++) {
+        array[NAVIO_DP_LINHA_INICIAL + k][NAVIO_DP_COL_INICIAL + k] = 3;
+    }
+
+    // Navio na Diagonal Secundária
+    for (int k = 0; k < NAVIO_DS_TAMANHO; k++) {
+        array[NAVIO_DS_LINHA_INICIAL + k][NAVIO_DS_COL_INICIAL - k] = 3;
+    }
+
 }
 
-void EXIBIR_TABULEIRO(int array [10][10]){
+void EXIBIR_TABULEIRO(int array [TAMANHO][TAMANHO]){
 
     printf(">---> === TABULEIRO BATALHA NAVAL === <---<\n");
 
@@ -70,11 +105,11 @@ void EXIBIR_TABULEIRO(int array [10][10]){
     printf("   | A B C D E F G H I J\n");
     printf("---+---------------------\n");
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < TAMANHO; i++) {
 
         printf("%2d | ", i + 1);
         
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < TAMANHO; j++) {
 
             if (j <= 8)
             {
@@ -97,15 +132,16 @@ void EXIBIR_TABULEIRO(int array [10][10]){
 
 }
 
-int main() {
-    
+int main(){
+
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    int tabuleiro [10][10];
-
-    DEFINIR_TAMANHO_TABULEIRO(tabuleiro);
-    DEFINIR_LOCAL_NAVIO(tabuleiro);
-    EXIBIR_TABULEIRO(tabuleiro);
+    int tabuleiro [TAMANHO][TAMANHO];
+    
+    DEFINIR_TAMANHO_TABULEIRO(tabuleiro); // Colocar o vator 0 para cada posição da matriz.
+    DEFINIR_LOCAL_NAVIO(tabuleiro); // Colocar cada navio no tabuleiro.
+    EXIBIR_TABULEIRO(tabuleiro); // Exibir o tabuleiro após a definição de cada local do tabuleiro.
 
     return 0;
+
 }
